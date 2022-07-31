e419a555f5c12cc32da5230afa804437efe5665f
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2022-07-31 03:56:05 -0400

    se modifico y termino las querys

--- semana4/controllers/game.js
+++ semana4/controllers/game.js
@@ -4,29 +4,32 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/player');
 const State = require('../models/stateGame');
+var gamers_array = [];
+var state_array = [];
+var lastState;
 
 router.get('/:id', function (req, res, next) {
   var idForGame = req.params.id;
   State.find(
-    
-      { idGame: req.params.id }
-    ).exec((err, teachers) => {
-      if (err) {
-        res.send(err)
-      }
-      if (teachers) {
-        groups_promises = teachers.map(function(idGames){
-          return User.find({idGame: req.params.id}).then((groups) => {
-            console.log("Winner "+teachers);
-            res.json({
-              gamers: groups,
-              Winners: teachers
-            });
-            return;
-          });
+
+    { idGame: req.params.id }
+  ).exec((err, teachers) => {
+    if (err) {
+      return res.send(err)
+    }
+    if (teachers) {
+      groups_promises = teachers.map(function(idForGame) {
+        return User.find({ idGame: req.params.id }).then((groups) => {
+          gamers_array = groups;
+          state_array = teachers;
+          lastState = state_array.length-1;
         });
-      }
+      });
+      return Promise.all(groups_promises).then(()=>{
+        res.json({idGame: idForGame, gamers: gamers_array, winner: state_array[lastState]});
     });
+    }
+  });
 });
 
 router.get('/:id/winner', function (req, res, next) {
@@ -34,7 +37,7 @@ router.get('/:id/winner', function (req, res, next) {
     idGame: req.params.id
   }, function callback(error, a) { //me permite acceder a los valores de la coleccion ejem: a.name datos dentro de la coleccion
     res.json(a)
-  })
+  }).sort({ '_id': -1 })
 });
 
 module.exports = router;
