d3eb5b3a6c4936f6f40e9fcc786dd05cabf81988
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2022-07-30 23:11:59 -0400

    cambios n game.js

--- semana4/controllers/game.js
+++ semana4/controllers/game.js
@@ -4,38 +4,36 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/player');
 const State = require('../models/stateGame');
-var hola;
+
 router.get('/:id', function (req, res, next) {
-  idForGame = req.params.id;
-  User.aggregate(
-    [
-      { $match: { idGame: req.params.id } }
-    ]).exec((err, result) => {
+  var idForGame = req.params.id;
+  State.find(
+    
+      { idGame: req.params.id }
+    ).exec((err, teachers) => {
       if (err) {
         res.send(err)
       }
-      if (result) {
-        res.json({
-          idGame: req.params.id,
-          gamer: result,
-          inProgress: statusGamer,
-          winner: { id: idWinner, name: nameGamer }
+      if (teachers) {
+        groups_promises = teachers.map(function(idGames){
+          return User.find({idGame: req.params.id}).then((groups) => {
+            console.log("Winner "+teachers);
+            res.json({
+              gamers: groups,
+              Winners: teachers
+            });
+            return;
+          });
         });
       }
     });
-    
 });
 
 router.get('/:id/winner', function (req, res, next) {
-  // const data = State.find({idGame: req.params.id})
-  // data
-  // .then(result => res.json(result))
-  // .catch(err => console.log(err));
-   State.findOne({ 
+  State.findOne({
     idGame: req.params.id
-  }, function callback(error, a) {
-    console.log(a.name);
-    console.log(a.inProgress);
+  }, function callback(error, a) { //me permite acceder a los valores de la coleccion ejem: a.name datos dentro de la coleccion
+    res.json(a)
   })
 });
 
